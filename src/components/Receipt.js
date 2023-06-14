f3112b6cd9fef97dad536d0d5e0e8144d9c411ed
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-14 19:01:14 +0100

    new receipt

--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -1,8 +1,15 @@
 import React from "react"
 
 const Receipt = ({ shopping }) => {
+  // Calculate the total price
+  const totalPrice = shopping.reduce(
+    (total, item) => total + item.groceryItem.price * item.quantity,
+    0
+  )
+
   return (
     <div className="receipt">
+      <h2>Receipt</h2>
       <ul>
         {shopping.map((item, index) => (
           <li key={index}>
@@ -13,6 +20,7 @@ const Receipt = ({ shopping }) => {
           </li>
         ))}
       </ul>
+      <p className="total-price">Total Price: ${totalPrice.toFixed(2)}</p>
     </div>
   )
 }
