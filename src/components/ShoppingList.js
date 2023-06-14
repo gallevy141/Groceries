015e9d95f22dadbead1a5b907004e3d6e3df08b9
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-14 17:26:07 +0100

    fixed shopping list component

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,49 +1,49 @@
 import React from "react"
-import ShoppingItem from "./ShoppingItem"
+import GroceryItem from "./GroceryItem"
 
 const ShoppingList = () => {
   const shopping = [
     {
-        name: "Apple",
-        price: 2.00,
-        quantity: 5,
-        thumbnail: "item1.png",
+      name: "Apple",
+      price: 2.00,
+      quantity: 5,
+      thumbnail: "item1.png",
     },
     {
-        name: "Orange",
-        price: 2.00,
-        quantity: 3,
-        thumbnail: "item2.png",
+      name: "Orange",
+      price: 2.00,
+      quantity: 3,
+      thumbnail: "item2.png",
     },
     {
-        name: "Strawberry",
-        price: 1.50,
-        quantity: 15,
-        thumbnail: "item3.png",
+      name: "Strawberry",
+      price: 1.50,
+      quantity: 15,
+      thumbnail: "item3.png",
     },
     {
-        name: "Peach",
-        price: 1.99,
-        quantity: 8,
-        thumbnail: "item4.png",
+      name: "Peach",
+      price: 1.99,
+      quantity: 8,
+      thumbnail: "item4.png",
     },
     {
-        name: "Lychee",
-        price: 0.60,
-        quantity: 58,
-        thumbnail: "item5.png",
+      name: "Lychee",
+      price: 0.60,
+      quantity: 58,
+      thumbnail: "item5.png",
     },
     {
-        name: "Raspberry",
-        price: 0.50,
-        quantity: 78,
-        thumbnail: "item6.png",
+      name: "Raspberry",
+      price: 0.50,
+      quantity: 78,
+      thumbnail: "item6.png",
     },
     {
-        name: "Pineapple",
-        price: 3.55,
-        quantity: 8,
-        thumbnail: "item7.png",
+      name: "Pineapple",
+      price: 3.55,
+      quantity: 8,
+      thumbnail: "item7.png",
     }
   ]
 
@@ -52,7 +52,7 @@ const ShoppingList = () => {
       <h2>Shopping List</h2>
       <ul>
         {shopping.map((item, index) => (
-          <ShoppingItem key={index} item={item} />
+          <GroceryItem key={index} item={item.groceryItem} quantity={item.quantity} />
         ))}
       </ul>
     </div>
