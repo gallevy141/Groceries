5e3ebcdcbb18eefe8499f9ab713dcfddba35be83
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-14 17:09:18 +0100

    Implement GroceryList component

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -1,19 +1,62 @@
 import React from "react"
+import GroceryItem from "./GroceryItem"
 
-const GroceryList = ({groceries, onGroceryClick}) => {
+const GroceryList = () => {
+  // Initialize the groceries array with grocery items
+  const groceries = [
+    {
+      name: "Apple",
+      price: 2.00,
+      quantity: 5,
+      thumbnail: "item1.png",
+    },
+    {
+      name: "Orange",
+      price: 2.00,
+      quantity: 3,
+      thumbnail: "item2.png",
+    },
+    {
+        name: "Strawberry",
+        price: 1.50,
+        quantity: 15,
+        thumbnail: "item3.png",
+      },
+      {
+        name: "Peach",
+        price: 1.99,
+        quantity: 8,
+        thumbnail: "item4.png",
+      },
+      {
+        name: "Lychee",
+        price: 0.60,
+        quantity: 58,
+        thumbnail: "item5.png",
+      },
+      {
+        name: "Raspberry",
+        price: 0.50,
+        quantity: 78,
+        thumbnail: "item6.png",
+      },
+      {
+        name: "Pineapple",
+        price: 3.55,
+        quantity: 8,
+        thumbnail: "item7.png",
+      }
+  ]
+
   return (
-    <ul id="groceries">
-      {groceries.map((item, index) => (
-        <li 
-          key={index}
-          className={item.quantity === 0 ? 'out-of-stock' : ''}
-          onClick={() => onGroceryClick(index)}
-        >
-          <img src={item.thumbnail} alt={item.name} />
-          <span>{item.name} - ${item.price.toFixed(2)} (x{item.quantity})</span>
-        </li>
-      ))}
-    </ul>
+    <div className="grocery-list">
+      <h2>Groceries</h2>
+      <ul>
+        {groceries.map((grocery, index) => (
+          <GroceryItem key={index} grocery={grocery} />
+        ))}
+      </ul>
+    </div>
   )
 }
 
