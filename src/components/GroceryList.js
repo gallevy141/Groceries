9418450d1b89896b190356592185d8cf3b216d34
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-14 17:30:31 +0100

    fixed grocerylist component

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -1,59 +1,13 @@
 import React from "react"
 import GroceryItem from "./GroceryItem"
 
-const GroceryList = () => {
-  // Initialize the groceries array with grocery items
-  const groceries = [
-    {
-      name: "Apple",
-      price: 2.00,
-      quantity: 5,
-      thumbnail: "item1.png",
-    },
-    {
-      name: "Orange",
-      price: 2.00,
-      quantity: 3,
-      thumbnail: "item2.png",
-    },
-    {
-        name: "Strawberry",
-        price: 1.50,
-        quantity: 15,
-        thumbnail: "item3.png",
-      },
-      {
-        name: "Peach",
-        price: 1.99,
-        quantity: 8,
-        thumbnail: "item4.png",
-      },
-      {
-        name: "Lychee",
-        price: 0.60,
-        quantity: 58,
-        thumbnail: "item5.png",
-      },
-      {
-        name: "Raspberry",
-        price: 0.50,
-        quantity: 78,
-        thumbnail: "item6.png",
-      },
-      {
-        name: "Pineapple",
-        price: 3.55,
-        quantity: 8,
-        thumbnail: "item7.png",
-      }
-  ]
-
+const GroceryList = ({ groceries, onGroceryClick }) => {
   return (
     <div className="grocery-list">
       <h2>Groceries</h2>
       <ul>
-        {groceries.map((grocery, index) => (
-          <GroceryItem key={index} grocery={grocery} />
+        {groceries.map((item, index) => (
+          <GroceryItem key={index} item={item} onGroceryClick={onGroceryClick} />
         ))}
       </ul>
     </div>
