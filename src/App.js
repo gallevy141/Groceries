3d68723921f7e89aafefb060141536a178624441
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-15 15:25:14 +0100

    added useState to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useState } from 'react';
 
 import Header from './components/Header'
 import Footer from './components/Footer'
