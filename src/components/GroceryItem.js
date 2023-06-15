51195e6d4c9a99e91ded72f1399250379a9b1b83
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-15 15:41:59 +0100

    updated groceryitem changing item

--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -3,18 +3,18 @@ import React from "react"
 const GroceryItem = ({ item, onGroceryClick }) => {
   const handleClick = () => {
     if (onGroceryClick) {
-      onGroceryClick(item);
+      onGroceryClick(item.groceryItem)
     }
   }
-  
+
   return (
     <li
-      className={item.quantity === 0 ? "out-of-stock" : ""}
+      className={item.groceryItem.quantity === 0 ? "out-of-stock" : ""}
       onClick={handleClick}
     >
-      <img src={item.thumbnail} alt={item.name} />
+      <img src={item.groceryItem.thumbnail} alt={item.groceryItem.name} />
       <span>
-        {item.name} - ${item.price.toFixed(2)} (x{item.quantity})
+        {item.groceryItem.name} - ${item.groceryItem.price.toFixed(2)} (x{item.quantity})
       </span>
     </li>
   )
