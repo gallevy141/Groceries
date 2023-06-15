4941c2c78a9e218bea7fcc9e3ede0e96734654e1
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-15 15:27:19 +0100

    removing react import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React, { useState } from 'react';
 
 import Header from './components/Header'
 import Footer from './components/Footer'
