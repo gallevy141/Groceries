7f84514ecd29ff3cad6f46aa1d0dee9132560730
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-15 14:44:23 +0100

    completed Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer>
-      {/* Footer content */}
+    <footer className="footer mt-auto py-3 bg-primary">
+      <div className="container">
+        <span className="text-white">Copyright © 2023 - Grocery Shopping</span>
+      </div>
     </footer>
   )
 }
