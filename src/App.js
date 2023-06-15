a9b00e3744992850ab02414d77cfcbcb6557c6ed
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-15 14:35:17 +0100

    implementing app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './Header'
+import Footer from './Footer'
+import GroceryList from './GroceryList'
+import OrderSummary from './OrderSummary'
+import ShoppingList from './ShoppingList'
+import Receipt from './Receipt'
 
-function App() {
+const App = () => {
+  const initialGroceries = [
+    { name: "Apple", price: 1.00, quantity: 10, thumbnail: "apple.png" },
+    { name: "Banana", price: 0.50, quantity: 20, thumbnail: "banana.png" },
+    { name: "Cherry", price: 2.00, quantity: 15, thumbnail: "cherry.png" },
+    { name: "Durian", price: 5.00, quantity: 5, thumbnail: "durian.png" },
+    { name: "Elderberry", price: 1.50, quantity: 25, thumbnail: "elderberry.png" },
+    { name: "Fig", price: 1.75, quantity: 10, thumbnail: "fig.png" },
+    { name: "Grapefruit", price: 2.50, quantity: 30, thumbnail: "grapefruit.png" },
+  ]
+
+  const [groceries, setGroceries] = useState(initialGroceries)
+  const [shopping, setShopping] = useState([])
+
+  const handleGroceryClick = (item) => {
+    setGroceries(
+      groceries.map((grocery) =>
+        grocery.name === item.name
+          ? { ...grocery, quantity: grocery.quantity - 1 }
+          : grocery
+      )
+    );
+    setShopping([...shopping, { ...item, quantity: 1 }])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Header />
+      <GroceryList groceries={groceries} onGroceryClick={handleGroceryClick} />
+      <OrderSummary shopping={shopping} />
+      <ShoppingList shopping={shopping} />
+      <Receipt shopping={shopping} />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
